c9db14061dd9863bc899f0104ddeebc802471bf5
bio8oid <8bollod8@mail.co.uk>
2020-10-15 01:43:09 +0200

    handle history display after percentage count

--- js/script.js
+++ js/script.js
@@ -2,80 +2,191 @@
 'use strict';
 (function () {
 
-    // const historyDisplay = document.getElementById('history');
-    // historyDisplay.value = 'test'
-
-    // const display = document.getElementById('main') 
-    // display.value = 1
-
     class Calculator {
         constructor() {
 
             this.currentNumber = '',
-            this.resultOfTwoNumbers = 0,
-            this.operand = '',
-            this.operandsHistory = [],
-            this.displayHistory = [],
-            this.numbersHistory = [],
-            this.totalCharactersHistory = [],
+                this.resultOfTwoNumbers = 0,
+                this.operand = '',
+                this.operandStatus = false,
+                this.operandsHistory = [],
+                this.displayHistory = [],
+                this.numbersHistory = [],
+                this.totalCharactersHistory = [],
 
-            this.historyDisplay = document.getElementById('history'),
-            this.display = document.getElementById('main'),
-            this.display.value = 0
+                this.historyDisplay = document.getElementById('history'),
+                this.display = document.getElementById('main'),
+                this.display.value = 0
         }
 
-        handleDisplays = x => {
-            this.historyDisplay.value = this.totalCharactersHistory.toString().replace(/,/g, ' ');
-            if (typeof x === 'string') this.display.value = x;
-        }
-
-        numbersHistoryHandle = () => {
+        numbersHistoryHandle = num => {
             if (this.currentNumber !== '') {
                 this.numbersHistory.push(Number(this.currentNumber))
                 this.totalCharactersHistory.push(Number(this.currentNumber));
+            } else {
+                this.numbersHistory.push(0)
+                // this.numbersHistory.push(num)
             }
         }
 
-        operandsHistoryHandle = () => {
-            this.operandsHistory.push(this.operand)
+        operandsHistoryHandle = operand => {
+            this.operandsHistory.push(operand)
+            this.operandStatus = true;
         }
 
         handleNumbers = e => {
             this.currentNumber += e.target.value;
-            console.log('this.currentNumber:', this.currentNumber)
+            // this.numbersHistoryHandle();
             this.handleDisplays(this.currentNumber);
+            this.operandStatus = false;
+        }
+
+        calculation = operand => {
+            console.log('calculation-operand:', operand)
+
+            let firstNumber = this.numbersHistory[0];
+            let secondNumber = this.numbersHistory[1];
+
+            console.log('firstNumber:', firstNumber)
+            console.log('secondNumber:', secondNumber)
+
+            if (this.operandStatus === true && this.numbersHistory.length > 1) {
+                // if (operand === '=' && this.operandsHistory.length > 1 ) {
+                //     operand = this.operandsHistory[this.operandsHistory.length - 2]
+                // } else {
+                //     operand = this.operandsHistory[this.operandsHistory.length - 1]
+                // }
+                console.log('operand-after:', operand)
+
+                switch (operand) {
+                    case '+':
+                        // console.log('case +:', firstNumber + secondNumber)
+                        this.resultOfTwoNumbers = firstNumber + secondNumber;
+                        // console.log('this.resultOfTwoNumbers:', this.resultOfTwoNumbers)
+                        this.handleDisplays(this.resultOfTwoNumbers);
+                        break;
+                    case '-':
+                        this.resultOfTwoNumbers = firstNumber - secondNumber;
+                        this.handleDisplays(this.resultOfTwoNumbers);
+                        break;
+                    case '*':
+                        this.resultOfTwoNumbers = firstNumber * secondNumber;
+                        this.handleDisplays(this.resultOfTwoNumbers);
+                        break;
+                    case '/':
+                        this.resultOfTwoNumbers = firstNumber / secondNumber;
+                        this.handleDisplays(this.resultOfTwoNumbers);
+                        break;
+                    case '%':
+                        this.resultOfTwoNumbers = ((firstNumber / 100) * secondNumber);
+                        this.handleDisplays(this.resultOfTwoNumbers);
+                        // this.historyDisplay.value = this.resultOfTwoNumbers
+                        // this.currentNumber = this.resultOfTwoNumbers;
+                        break;
+                    case '+-':
+                        if (Math.sign(this.display.value) === 1 ) {
+
+                            this.display.value = -Math.abs(this.display.value)
+                            console.log('this.display.value:', this.display.value)
+
+                            this.historyDisplay.value = this.display.value
+
+                            this.numbersHistory = this.display.value
+
+                            this.totalCharactersHistory.splice(this.totalCharactersHistory.length -1, 1, this.display.value)
+
+                        } else {
+
+                            this.display.value = Math.abs(this.display.value);
+
+                            this.historyDisplay.value = this.display.value;
+
+                            this.totalCharactersHistory.splice(this.totalCharactersHistory.length -1, 1, this.display.value)
+
+                        }
+                        this.operandStatus = false;
+                        break;
+                    default:
+                        console.log(`no prerand error`);
+                }
+                if ( this.numbersHistory.length > 1 ) {
+                    this.numbersHistory.splice(0, 2, this.resultOfTwoNumbers);
+                } else {
+                    this.numbersHistory = this.numbersHistory;
+                }
+            }
         }
 
         handleOperators = e => {
 
-            // store numbers
-            this.numbersHistoryHandle()
-            this.currentNumber = '';
 
-            // store operators
+            // set and add entered numbers to memory
+
+            // if (e.target.value === 'C') {
             if (e.target.value === 'C') {
+
                 this.clear();
+
             } else {
-                this.operand = e.target.value
-            }
-            const charactersArr = this.totalCharactersHistory
-            const lastCharacter = charactersArr[charactersArr.length - 1]
+               
+                this.operand = e.target.value;
+                this.operandsHistoryHandle(this.operand);
+                
+                // add entered numbers to memory
+                this.numbersHistoryHandle()
+                this.currentNumber = '';
+    
+                // if (this.operandsHistory[this.operandsHistory.length - 1] === '%') {
+                //                 console.log('kutas')
+                //                 this.handleDisplays(this.resultOfTwoNumbers, this.resultOfTwoNumbers);
 
-            if (typeof lastCharacter === 'number') {
-                charactersArr.push(this.operand);
+                if (this.operandsHistory.length > 1 && this.operand !== '%' && this.operandsHistory[this.operandsHistory.length - 2] !== '%') {
+
+                    console.log('fired')
+                    this.calculation(this.operandsHistory[this.operandsHistory.length -2]);
+                    // this.operandsHistory.splice(0, 1);
+                } else {
+                    this.calculation(this.operand);
+                        
+                    // } 
+                }
+            }
+
+            const ArrCharacters = this.totalCharactersHistory
+            const lastArrCharacter = ArrCharacters[ArrCharacters.length - 1]
+
+            if (typeof lastArrCharacter === 'number') {
+                if (this.operand !== '+-') ArrCharacters.push(this.operand);
             } else {
-                charactersArr.splice(charactersArr.length - 1, 1, this.operand)
+                if (this.operandsHistory[this.operandsHistory.length - 2] === '%') {
+                    ArrCharacters.splice(ArrCharacters.length - 1, 1, this.operand)
+                    this.totalCharactersHistory = [this.resultOfTwoNumbers, this.operand]
+                } else {
+                    ArrCharacters.splice(ArrCharacters.length - 1, 1, this.operand)
+                }
+
             }
-
-
+  
             this.handleDisplays();
 
             console.log('operand:', this.operand)
             console.log('calky:', calky)
         }
 
+
         handleButtons = e => {
 
+        }
+
+        handleDisplays = (x,y) => {
+            // if (y !== undefined) {
+            //     console.log('y:', y)
+            //     this.historyDisplay.value = this.totalCharactersHistory.toString().replace(/,/g, '0');
+            // } else {
+                this.historyDisplay.value = this.totalCharactersHistory.toString().replace(/,/g, ' ');
+                // this.display.value = [];
+            // }
+            if (x !== undefined) this.display.value = x;
         }
 
         clear = () => {
@@ -91,7 +202,6 @@
         }
     }
 
-
     const calky = new Calculator();
 
     const allButtons = document.querySelectorAll('button');
@@ -101,7 +211,6 @@
     for (const button of allButtons) button.addEventListener("click", e => calky.handleButtons(e));
     for (const button of numberButtons) button.addEventListener("click", e => calky.handleNumbers(e));
     for (const button of operatorButtons) button.addEventListener("click", e => calky.handleOperators(e));
-
 
     //     const resultDisplay = document.getElementById('result')
     //     const allButtons = document.querySelectorAll('button')
